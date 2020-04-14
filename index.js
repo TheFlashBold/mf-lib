2f8bda095e1df301314515be99c644df677480dd
tycho <tycho.holzer@gmail.com>
2020-04-14 21:44:38 +0200

    added script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Application = require('./lib/Application');
 const request = require('request-promise');
 const Module = require('./lib/Module');
+const Script = require('./lib/Script');
 const DeepMerge = require('deepmerge');
 const readdirp = require('readdirp');
 const Promise = require('bluebird');
@@ -9,6 +10,7 @@ const mpath = require('mpath');
 module.exports = {
     Application: Application,
     Module: Module,
+    Script: Script,
     Modules: {
         DeepMerge,
         readdirp,
